b4009064ede05711d6163ebfc0e6416beb0487d2
Lokesh Hariramani <lakshya1hariramani@gmail.com>
2025-02-04 16:33:07 +0530

    Update page.tsx

--- app/Users/page.tsx
+++ app/Users/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
   return (
     <div className="page-container">
       <head>
-      <meta name="viewport" content="width=1280, initial-scale=0.15, maximum-scale=1.0" /> {/* Fixed width for desktop */}
+      <meta name="viewport" content="width=1280, initial-scale=0.2, maximum-scale=1.0" /> {/* Fixed width for desktop */}
         <link
           href="https://fonts.googleapis.com/css2?family=Geist:wght@100..900&display=swap"
           rel="stylesheet"
