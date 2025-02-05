3e961e70479ca971c7a4350e2bdc94d3e1150d7f
Lokesh Hariramani <lakshya1hariramani@gmail.com>
2025-02-05 19:40:56 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import Image from "next/image";
-import "./login.css";
+import "./styles.css";
 
 export default function Home() {
   const [shouldFadeRoadmap, setShouldFadeRoadmap] = useState(false);
